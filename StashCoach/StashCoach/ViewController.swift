c7d25530ae5b211858a1fbf8471cb3e73cd94d1f
Edgar Barocio <edgar.barocio@gmail.com>
2020-04-07 20:27:48 -0700

    Create project

---
+++ StashCoach/StashCoach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StashCoach
+//
+//  Created by Edgar Barocio on 4/6/20.
+//  Copyright © 2020 Edgar Barocio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
